9b45d3eeba82e91b2d4b21a6255ed46cdf542927
SwitchAxe <sofiacerasuoli@gmail.com>
2021-06-23 15:51:10 +0200

    major refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 use rand::Rng;
 use std::{thread, time};
-const ROWS: usize = 20;
-const COLS: usize = 20;
 
-#[derive(Clone)]
-#[derive(Copy)]
-struct Cell {
-    old_state: u8, //1 for the head, 2 for the trail, 0 for the dead cells
-    new_state: u8, // same here
-}
-
+const ROWS: usize = 50;
+const COLS: usize = 50;
 
 enum Directions {
     Left,
@@ -18,12 +11,25 @@ enum Directions {
     Down,
     None,
 }
+// #[derive(Clone)]
+// #[derive(Copy)]
+// struct Cell {
+//     //1 for the head, 2 for the trail, 0 for a dead cell
+//     old_state: usize,
+//     new_state: usize,
+// }
 
 fn main() {
-    let mut grid = [[Cell {old_state: 0, new_state: 0,}; ROWS]; COLS];
-    grid[ROWS/2][COLS/2].old_state = 1; //initial head
-    let one_second = time::Duration::from_millis(1000);
-    let mut counter = 0;
+    //let mut grid = [[Cell {old_state: 0, new_state: 0}; ROWS]; COLS];
+    let mut trail: Vec<(usize, usize)> = Vec::new();
+    //first head of the automaton
+    let head = (ROWS/2, COLS/2);
+    trail.push(head);
+    //setting the corrisponding grid entry to true:
+    //grid[head.0][head.1].old_state = 1;
+    let interval = time::Duration::from_millis(100);
+    let mut generations: usize = 1;
+    print!("\x1b[2J");
     loop {
         let direction = match rand::thread_rng().gen_range(1..5) {
             1 => Directions::Left,
@@ -32,80 +38,43 @@ fn main() {
             4 => Directions::Down,
             _ => Directions::None,
         };
-        print!("\x1b[2J\x1b[0;0H"); //clears the screen on Linux
-        print_grid(&grid);
-        update_grid(&mut grid, &direction);
-        update_states(&mut grid);
-        println!("Generations = {}", counter);
-        counter = counter + 1;
-        thread::sleep(one_second);
+        //moves the cursor on the top left corner of the screen on linux
+        print!("\x1b[0;0H");
+        println!("Generations: {}", generations);
+        print_grid(&trail);
+        trail.push(get_next_head(&trail[trail.len() - 1], &direction));
+        thread::sleep(interval);
+        generations = generations + 1;
     }
 }
 
-fn update_grid(arr: &mut [[Cell; ROWS]; COLS], direction: &Directions) {
-    for x in 1..ROWS {
-        for y in 1..COLS {
-            match arr[x][y].old_state {
-                1 => match check_neighborhood(arr, x, y) {
-                         true => match direction {
-                                     Directions::Left => {
-                                         arr[x-1][y].new_state = 1;
-                                         arr[x][y].new_state = 2;
-                                     },
-                                     Directions::Right => {
-                                         arr[x+1][y].new_state = 1;
-                                         arr[x][y].new_state = 2;
-                                     },
-                                     Directions::Up => {
-                                         arr[x][y-1].new_state = 1;
-                                         arr[x][y].new_state = 2;
-                                     },
-                                     Directions::Down => {
-                                         arr[x][y+1].new_state = 1;
-                                         arr[x][y].new_state = 2;
-                                     },
-                                     _ => {},
-                                 },
-                         false => continue,
-                     },
-                 2 => continue,
-                 0 => continue,
-                 _ => continue, //unused, it's not possible for a cell to be in this state
-            }
-        }
-    }
+fn get_next_head(head: &(usize, usize),
+                 direction: &Directions) -> (usize, usize) {
+    //function to get the next head of the automaton
+    return match direction {
+        Directions::Left => (head.0 - 1, head.1),
+        Directions::Right => (head.0 + 1, head.1),
+        Directions::Up => (head.0, head.1 - 1),
+        Directions::Down => (head.0, head.1 + 1),
+        _ => (head.0, head.1),
+    };
 }
 
-fn update_states(arr: &mut [[Cell; ROWS];COLS]) {
-    for x in 1..ROWS {
-        for y in 1..COLS {
-            arr[x][y].old_state = arr[x][y].new_state;
-        }
-    }
-}
+// fn print_grid(grid: &[[Cell; ROWS]; COLS]) {
+//     for x in 1..ROWS {
+//         for y in 1..COLS {
+//             println!("{}", if grid[x][y].old_state == 0 {
+//                 " "
+//             } else {
+//                 "o"
+//             });
+//         }
+//     }
+// }
 
-fn print_grid(arr: &[[Cell; ROWS]; COLS]) {
-    for x in 1..ROWS {
-        for y in 1..COLS {
-            print!("{}", if arr[x][y].old_state == 0 {
-                " "
-            } else {
-                "o"
-            })
-        }
-        print!("\n");
-    }
-}
-
-fn check_neighborhood(arr: &[[Cell;ROWS];COLS], xindex: usize, yindex: usize) -> bool {
-    if xindex > 1 && yindex > 1 && xindex < ROWS - 1 && yindex < COLS - 1 {
-        return arr[xindex-1][yindex].old_state != 0 ||
-            arr[xindex][yindex-1].old_state != 0 ||
-            arr[xindex+1][yindex].old_state != 0 ||
-            arr[xindex][yindex+1].old_state != 0 ||
-            arr[xindex][yindex].old_state != 0;
-
-    } else {
-        return false;
+fn print_grid(trail: &Vec<(usize, usize)>)
+{
+    for i in 0..trail.len() {
+        print!("\x1b[{};{}Ho", trail[i].1, trail[i].0);
     }
 }
